937174775b98d348bf6789d9d6f47e89e5f198a8
Hao Hoang <haohx@pamysoft.com>
2024-09-23 23:55:34 +0700

    'update'

--- app/components/carcovers/StarRating.tsx
+++ app/components/carcovers/StarRating.tsx
@@ -2,13 +2,13 @@ import starImgUrl from '~/assets/starimg.jpg';
 
 export function StarRating() {
     return <>
-        <div className="mt-[0px] block">
+        <div className="mt-[5px] block text-[14px]">
             <div className="inline">
-                <img className="inline" src={starImgUrl} alt="Rating" width="16" height="16" />
-                <img className="inline" src={starImgUrl} alt="Rating" width="16" height="16" />
-                <img className="inline" src={starImgUrl} alt="Rating" width="16" height="16" />
-                <img className="inline" src={starImgUrl} alt="Rating" width="16" height="16" />
-                <img className="inline" src={starImgUrl} alt="Rating" width="16" height="16" />
+                <img className="inline" src={starImgUrl} alt="Rating" width="14" height="14" />
+                <img className="inline" src={starImgUrl} alt="Rating" width="14" height="14" />
+                <img className="inline" src={starImgUrl} alt="Rating" width="14" height="14" />
+                <img className="inline" src={starImgUrl} alt="Rating" width="14" height="14" />
+                <img className="inline" src={starImgUrl} alt="Rating" width="14" height="14" />
             </div>
             <div className="inline">
                 (<b>10785</b> Reviews) <span className="verified hidden-xs">| Verified Customer Reviews</span> | 4.8/5 Positive
